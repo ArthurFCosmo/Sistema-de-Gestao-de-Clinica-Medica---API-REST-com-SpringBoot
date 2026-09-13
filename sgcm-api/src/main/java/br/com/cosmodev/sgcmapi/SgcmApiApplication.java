3ebe62e2e503dda257ecfbaea84e2d133eff7b95
Arthur Filipe Cosmo <arthurcosmoservices@gmail.com>
2026-09-13 11:55:09 -0300

    Adição do pacote de estruturas do Spring Initializr

---
+++ sgcm-api/src/main/java/br/com/cosmodev/sgcmapi/SgcmApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cosmodev.sgcmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcmApiApplication.class, args);
+	}
+
+}
---
+++ sgcm-api/src/test/java/br/com/cosmodev/sgcmapi/SgcmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cosmodev.sgcmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
